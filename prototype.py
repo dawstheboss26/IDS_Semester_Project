571496e195e49dd638bcc4969cdcf9feb3624356
Ashley Yeung <ayeung@nd.edu>
2023-04-13 22:56:39 -0500

    cast in progress - error right now

--- prototype.py
+++ prototype.py
@@ -5,23 +5,26 @@
 #from tensorflow import keras
 
 import pandas as pd 
+import pprint
+import json
 
 import sys
 
 meta = pd.read_csv("data/movies_metadata.csv")
+credit = pd.read_csv("data/credits.csv")
 
 fullVector = []
 
-# add movies to vector space
+#* add movies to vector space
 for movie in meta["original_title"]: 
     fullVector.append([movie])
 
-# add ratings and normalize them -- highest is 10
+#* add ratings and normalize them -- highest is 10
 for count, rating in enumerate(meta["vote_average"]):
     rating /= 10
     fullVector[count].append(rating)
 
-# add year and normalize -- highest is 2020
+#* add year and normalize -- highest is 2020
 for count, year in enumerate(meta["release_date"]):
     if pd.isna(year):
         year = 0
@@ -34,7 +37,7 @@ for count, year in enumerate(meta["release_date"]):
 
     fullVector[count].append(year)
 
-# # add columns for 21 genres
+#* add columns for 21 genres
 # genres: Music, Family, Drama, Horror, War, Documentary, Adventure, TV Movie, Animation, Mystery, Action, Science Fiction
 #         Foreign, Comedy, Crime, Romance, History, Aniplex, Western, Fantasy, Thriller
 
@@ -59,10 +62,51 @@ for count, genres in enumerate(meta["genres"]):
                 genre_vector[num] = 1
 
     fullVector[count].extend(genre_vector)
-
-print(fullVector[0])
     
-# # add columns for cast (crop top X actors/actresses)
+#* add columns for cast (crop top X actors/actresses)
+cast_dict = dict()
+for count, cast in enumerate(credit["cast"]):
+    movie_cast = []
+    cast = cast.replace("'id':", "")
+    cast = cast.replace("'cast_id':", "")
+    cast = cast.replace("'character':", "")
+    cast = cast.replace("'credit_id':", "")
+    cast = cast.replace("'gender':", "")
+    cast = cast.replace("'order':", "")
+    cast = cast.replace("'profile_path':", "")
+    num_cast = cast.count(":")
+    cast = cast.split(":")
+
+    for i in range(num_cast):
+        c = cast[i + 1]
+        try:
+            c = c.split("'")[1]
+            if c == ",  ":
+                continue
+            movie_cast.append(c)
+        except:
+            continue
+
+    for actor in movie_cast:
+        cast_dict[actor] = cast_dict.setdefault(actor, 0) + 1
+
+sorted_cast_dict = dict(sorted(cast_dict.items(), key=lambda x:x[1], reverse=True))
+
+top_100_cast = list(sorted_cast_dict.keys())[0:100]
+
+for count, cast in enumerate(credit["cast"]):
+    cast_vector = [0] * 100
+    for num, actor in enumerate(top_100_cast):
+        if actor in cast:
+            cast_vector[num] = 1
+    # for i in range(len(fullVector)):
+    #     for actor in cast_vector:
+    #         fullVector[i].append(actor)
+    fullVector[count].extend(cast_vector)
+
+#! error: credits has 10 more rows than metadata --> have to match up with ID
+
+# print(fullVector[1])
 
 # # add columns for description using the USE API and stick on vector space -- crop length
 

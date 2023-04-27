789fc4edae425567db930ae3f94bda4a766768ee
dawstheboss26 <dflatley@nd.edu>
2023-04-26 22:58:24 -0400

    working openai API and classification

--- movieRecommender.py
+++ movieRecommender.py
@@ -1,8 +1,12 @@
 import json
 import editdistance
 import numpy as np
+import openai
+import os
 from numpy.linalg import norm
 from enum import Enum
+
+openai.api_key_path = "apikey.txt"
 
 class State(Enum):
     ANOTHER = 1
@@ -11,6 +15,7 @@ class State(Enum):
     SPECIFY_DIRECTOR = 4
     SPECIFY_ACTOR = 5
     ACCEPT = 6
+    UNK = 7
 
 def findTargetMovie():
     # get user input and use edit distance to get the most similar movie to the one they provide
@@ -64,27 +69,40 @@ def recommend(cosines, mIndex = 0):
     print(f'I would suggest: {rec}')
 
 def determineNextState():
-    uinput = input()
-    nextState = None
+    uinput = input("> ")
+    textClass = None
 
     # use ChatGPT API here for text classification -- send uinput to it
-    textClass = State.ANOTHER
+    chatInput = [{"role": "user", "content": "you are a sentence classifier. Sentences I give you will be one of these 7 classes: Class 1 is asking for another reccommendation. Class 2 is switching to a different movie to compare to. Class 3 is specifying genres. Class 4 is specifying directors. Class 5 is specifying actors. Class 6 is confirming the reccomendation. Classify a sentence as class 7 if it doesn't fit in classes 1, 2, 3, 4, 5, or 6. Respond with the format: <class number>. Here is the sentence:\n" + uinput}]
+    #chatInput = [{"role": "user", "content": "say 0"}]
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=chatInput)
+    reply = response['choices'][0]['message']['content']
+    #print(reply)
+    try:
+        theClass = int(reply)
+    except:
+        print("could not parse chatGPT classifier response")
+        return State.UNK
 
-    if textClass == State.ANOTHER:
-        return State.ANOTHER
-    elif textClass == State.SIMILAR_MOVIE:
-        return State.SIMILAR_MOVIE
-    elif textClass == State.SPECIFY_GENRE:
-        return State.SPECIFY_GENRE
-    elif textClass == State.SPECIFY_DIRECTOR:
-        return State.SPECIFY_DIRECTOR
-    elif textClass == State.SPECIFY_ACTOR:
-        return State.SPECIFY_ACTOR
-    else:
-        return State.ACCEPT
+    if reply == "1":
+        textClass = State.ANOTHER
+    if reply == "2":
+        textClass = State.SIMILAR_MOVIE
+    if reply == "3":
+        textClass = State.SPECIFY_GENRE
+    if reply == "4":
+        textClass = State.SPECIFY_DIRECTOR
+    if reply == "5":
+        textClass = State.SPECIFY_ACTOR
+    if reply == "6":
+        textClass = State.ACCEPT
+    if reply == "7":
+        textClass = State.UNK
+    
+    return textClass
+    
 
     # next states include: ANOTHER, SIMILAR_MOVIE, SPECIFY_GENRE, SPECIFY_DIRECTOR, SPECIFY_ACTOR, ACCEPT
-    return nextState
 
 # open the data from prototype.py (vector space, list of movies, and field lengths)
 fin = open('data/fullVectorData.json', 'r')
@@ -121,15 +139,18 @@ while (not accept):
     if nextState == State.ANOTHER:
         mIndex += 1
         recommend(cosines, mIndex)
-    elif textClass == State.SIMILAR_MOVIE:
+    elif nextState == State.SIMILAR_MOVIE:
         bestTitleIndex = findTargetMovie()
         cosines = calcCosineSim(bestTitleIndex)
         recommend(cosines, 0)
-    elif textClass == State.SPECIFY_GENRE:
+    elif nextState == State.SPECIFY_GENRE:
         continue
-    elif textClass == State.SPECIFY_DIRECTOR:
+    elif nextState == State.SPECIFY_DIRECTOR:
         continue
-    elif textClass == State.SPECIFY_ACTOR:
+    elif nextState == State.SPECIFY_ACTOR:
+        continue
+    elif nextState == State.UNK:
+        print("I'm sorry, I don't understand how that pertains to movies.")
         continue
     else:
         accept = 1
